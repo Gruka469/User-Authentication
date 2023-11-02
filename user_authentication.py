15858731c905e60911ba86424777bb0b4fa2c9b4
Gruka469 <lifetroll6@gmail.com>
2023-11-02 17:51:37 +0200

    Create user_authentication.py

---
+++ user_authentication.py
@@ -0,0 +1,40 @@
+user_database = {}
+
+def register_user(username, password):
+    if username not in user_database:
+        user_database[username] = password
+        print("User registered successfully")
+    else:
+        print("Username already exists try a different one.")
+
+
+def authenticate_user(username, password):
+    if username in user_database and user_database[username] == password:
+        return True
+    else:
+        return False
+    
+while True:
+    print("\nMenu:")
+    print("1. Register")
+    print("2. Authenticate")
+    print("3. Quit")
+
+    choice = input("Enter your choice (1/2/3): ")
+
+    if choice == "1":
+        username_input = input("Enter Username: ")
+        password_input = input("Enter Password: ")
+        register_user(username_input, password_input)
+    elif choice == "2":
+        username_input = input("Enter Username:")
+        password_input = input("Enter Password:")
+        if authenticate_user(username_input, password_input):
+            print("Authentication successful.")
+        else:
+            print("Authentication failed.")
+    elif choice == "3":
+        print("Goodbye")
+        break
+    else:
+        print("Invalid choice. Please select a valid option.")
